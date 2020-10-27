23a0f82b60ad6261a377ec4f99cbebb43bf86886
RaffaeleDoti <raffaele01.doti@studenti.unicam.it>
2020-10-27 12:33:24 +0100

    Init project

---
+++ GlobalizedApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+
+namespace GlobalizedApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        #region Attributes 
+        /// <summary>
+        /// String localizer istance.
+        /// </summary>
+        private readonly IStringLocalizer<WeatherForecastController> stringLocalizer;
+
+        /// <summary>
+        /// logger
+        /// </summary>
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        #endregion
+
+        #region Ctor 
+
+        /// <summary>
+        /// Ctor
+        /// </summary>
+        /// <param name="stringLocalizer"></param>
+        /// <param name="logger"></param>
+        public WeatherForecastController(IStringLocalizer<WeatherForecastController> stringLocalizer, ILogger<WeatherForecastController> logger)
+        {
+            //dependency injection 
+            this.stringLocalizer = stringLocalizer;
+            _logger = logger;
+        }
+
+        #endregion
+
+        #region Endpoints
+
+        [HttpGet]
+        [Route("/GetMessage")]
+        public async Task<IActionResult> GetMessage()
+        {
+            // getting current culture -> auto injected from default request culture provider via cookie,accept-language http header or "culture" http query string param
+            var currentCulture = CultureInfo.CurrentCulture;
+            // retrieve string from resx file in resources folder ( see microsoft documentation for resources naming conventions )
+            var message = stringLocalizer.GetString("MessaggioBenvenuto");
+            //returning message value
+            return Ok(message.Value);
+        }
+
+        #endregion
+    }
+}
---
+++ GlobalizedApi/Startup.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GlobalizedApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            #region GlobalizationService
+            //adding globalization and localization service
+            services.AddLocalization(options => options.ResourcesPath = "Resources");
+            //adding data annotazion localization.
+            services.AddMvc().AddDataAnnotationsLocalization();
+            #endregion 
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            #region GlobalizationConfiguration
+            var supportedCultures = new[] { "en", "it" };
+            var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[1])
+                .AddSupportedCultures(supportedCultures)
+                .AddSupportedUICultures(supportedCultures);
+
+            app.UseRequestLocalization(localizationOptions);
+            #endregion
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
